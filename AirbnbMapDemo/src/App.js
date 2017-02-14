108bd11a35bed1f147b2dac9d2ee07b3972eb554
zhulinpinyu <mulixiang199016@gmail.com>
2017-02-14 16:45:45 +0800

    show text on Top of Map

--- AirbnbMapDemo/src/App.js
+++ AirbnbMapDemo/src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 import MapView from 'react-native-maps'
 
 let id=0
@@ -27,24 +28,47 @@ export default class App extends Component {
 
   render() {
     return (
-      <MapView
-        style={{flex: 1}}
-        showsUserLocation={true}
-        followsUserLocation={true}
-        onLongPress={this.onMapPress.bind(this)}
-      >
-        {
-          this.state.markers.map(marker => {
-            return (
-              <MapView.Marker
-                key={marker.id}
-                coordinate={marker.coordinate}
-                pinColor={marker.color}
-              />
-            )
-          })
-        }
-      </MapView>
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          showsUserLocation={true}
+          followsUserLocation={true}
+          onLongPress={this.onMapPress.bind(this)}>
+          {
+            this.state.markers.map(marker => {
+              return (
+                <MapView.Marker
+                  key={marker.id}
+                  coordinate={marker.coordinate}
+                  pinColor={marker.color}
+                />
+              )
+            })
+          }
+        </MapView>
+        <View style={styles.label}>
+          <Text style={styles.labelText}>Map</Text>
+        </View>
+      </View>
     );
+  }
+}
+
+const styles = {
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-start'
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  label: {
+    flexDirection: 'row',
+    paddingTop: 60,
+    paddingLeft: 60,
+    backgroundColor: 'transparent'
+  },
+  labelText: {
+    fontSize: 20
   }
 }
